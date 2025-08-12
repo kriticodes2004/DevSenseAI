3815efbf7e9495b701b8b1a918c9b114ad485d96
kriticodes2004 <kritikhurana2004@gmail.com>
2025-08-13 04:17:19 +0530

    Add files via upload

---
+++ team.py
@@ -0,0 +1,6 @@
+TEAM_MEMBERS = {
+    "Frontend": "kriti025btece22@igdtuw.ac.in",
+    "Backend": "shivangisingh2314@gmail.com",
+    "Database": "saumyamathur2000@gmail.com",
+    "General": "kritikhurana2004@gmail.com"
+}
