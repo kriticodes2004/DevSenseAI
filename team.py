61d2a514e5cf9b60bc86895752c9ccaf7884aab0
kriticodes2004 <kritikhurana2004@gmail.com>
2025-08-17 04:39:10 +0530

    Delete all repo contents

--- team.py
+++
@@ -1,6 +0,0 @@
-TEAM_MEMBERS = {
-    "Frontend": "kriti025btece22@igdtuw.ac.in",
-    "Backend": "shivangisingh2314@gmail.com",
-    "Database": "saumyamathur2000@gmail.com",
-    "General": "kritikhurana2004@gmail.com"
-}
