415f02811f99e2dd44aacee2d1f0b6ccd2a3cb97
kriticodes2004 <kritikhurana2004@gmail.com>
2025-08-17 04:47:18 +0530

    Update embeddings.py

--- embeddings.py
+++ embeddings.py
@@ -3,7 +3,7 @@ from sentence_transformers import SentenceTransformer
 import numpy as np
 
 
-model = SentenceTransformer("all-MiniLM-L6-v2")  # 384-dim embeddings, small & fast
+model = SentenceTransformer("all-MiniLM-L6-v2")  
 
 def embed_text(text: str) -> np.ndarray:
 
